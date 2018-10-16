4ac1fa922f2031843c2dcf3edf35022743fc5461
karthik7dgl <44024211+karthik7dgl@users.noreply.github.com>
2018-10-16 16:13:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Welcome Karthi Kumar to Node js with Autodeployement Tool Using Jenknins !!!!!!");
+    response.end("Hi Karthik Welcome to Node js with Auto deployement Tool Using Jenknins !!!!!!");
 
 });
 
