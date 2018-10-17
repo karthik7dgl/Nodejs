580d85e0f0c3bb3a34018a4a2a42dde0b37f19bb
karthik7dgl <44024211+karthik7dgl@users.noreply.github.com>
2018-10-17 10:50:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hi Welcome to Node js with Auto deployement Tool Using Jenknins !!!!!!");
+    response.end("Hi js with Auto deployement Tool Using Jenknins !!!!!!");
 
 });
 
